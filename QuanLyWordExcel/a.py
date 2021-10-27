797a95c83f5646cd8d5b04a3372feaaf6aa6a574
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-10-27 21:15:28 +0700

    python

--- QuanLyWordExcel/a.py
+++ QuanLyWordExcel/a.py
@@ -4,17 +4,33 @@ file_new = open("student_new.csv",mode="w",encoding="utf-8-sig")
 
 header =file.readline()
 #.stip() là hàm loại bỏ kí tự xuống dòng ở cuối 
-file_new.write(header.strip() + ",Điểm trung bình, Học Lực")
+file_new.write(header.strip() + ",Điểm trung bình, Học Lực\n")
 
 # mỗi cái readline là đọc từng hàng 
 row = file.readline()
-row_list = row.split(",") 
 
-math = float(row_list[2])
-lit = float(row_list[3])
+while row != "":
+	pass	
+	row_list = row.split(",") 
 
-ave = (math+lit)/2
-# làm tròn 1 chữ số phần thập phân 
-ave = round(ave,1)
+	math = float(row_list[2])
+	lit = float(row_list[3])
 
-print(ave)
+
+	ave = (math+lit)/2
+	# làm tròn 1 chữ số phần thập phân 
+	ave = round(ave,1)
+	
+	rank = ""
+
+	if ave >= 8.0:
+		rank = "Giỏi"
+	elif ave >= 6.5 and ave < 8.0:
+		rank = "Tiên Tiến"
+	else:
+		rank + "Trung Bình"	
+	
+
+	row_new = row.strip() +"," + str(ave) +"," + rank + "\n"
+	file_new.write(row_new)
+	row = file.readline()
