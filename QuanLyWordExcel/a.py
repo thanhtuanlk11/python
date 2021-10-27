4064ac4a0f59a8e0fa77e4d2ca69e5368eda26a2
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-10-27 20:59:06 +0700

    python

--- QuanLyWordExcel/a.py
+++ QuanLyWordExcel/a.py
@@ -1,3 +1,20 @@
 # có 2 loại mode r= read , w= write
 file = open("student.csv",mode ="r", encoding="utf-8-sig") 
-print(file.readline())
+file_new = open("student_new.csv",mode="w",encoding="utf-8-sig")
+
+header =file.readline()
+#.stip() là hàm loại bỏ kí tự xuống dòng ở cuối 
+file_new.write(header.strip() + ",Điểm trung bình, Học Lực")
+
+# mỗi cái readline là đọc từng hàng 
+row = file.readline()
+row_list = row.split(",") 
+
+math = float(row_list[2])
+lit = float(row_list[3])
+
+ave = (math+lit)/2
+# làm tròn 1 chữ số phần thập phân 
+ave = round(ave,1)
+
+print(ave)
